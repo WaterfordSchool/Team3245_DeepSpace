a47fc5dcb11a4844ac534aefbb35342580426492
waterfordschool <lewiswilliams@comcast.net>
2019-02-02 11:49:58 -0500

    Updated PegPush by adding new Solenoid and Scoring method

--- TankDrive/src/main/java/frc/robot/subsystems/PegPush.java
+++ TankDrive/src/main/java/frc/robot/subsystems/PegPush.java
@@ -8,6 +8,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 /**
  * Add your docs here.
@@ -15,7 +18,14 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class PegPush extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-
+  //new solenoid called
+  DoubleSolenoid LittleDog = new DoubleSolenoid(20, 0, 1);
+  
+  public void DiscScore(){
+    LittleDog.set(DoubleSolenoid.Value.kForward);
+    Timer.delay(3);
+    LittleDog.set(DoubleSolenoid.Value.kReverse);
+  }
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
