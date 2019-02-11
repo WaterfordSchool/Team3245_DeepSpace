b4e8a6e4eac8f540f23ce446b591b3f76ea2c5d1
waterfordschool <justinjoslin@waterfordschool.org>
2019-02-11 12:29:20 -0700

    Added comments

--- CompCode/src/main/java/frc/robot/commands/CargoWrist.java
+++ CompCode/src/main/java/frc/robot/commands/CargoWrist.java
@@ -9,8 +9,14 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
-
+/**Class CargoWrist;
+ * The commands to pivot the cargo-pickup contraption are written here
+ * @author Some Nerd
+ * @see Robot.java OI.java Cargo.java
+ */
 public class CargoWrist extends Command {
+  /**The constructor method
+   */
   public CargoWrist() {
     requires(Robot.m_cargo);
     // Use requires() here to declare subsystem dependencies
@@ -19,23 +25,36 @@ public class CargoWrist extends Command {
 
   // Called just before this Command runs the first time
   @Override
+  /**The method called before CargoWrist.execute() runs for the first time
+   * Nothing to see here...
+   */
   protected void initialize() {
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
+  /**The method called when a CargoWrist instance is called
+   * @see Cargo.java Robot.java
+   */
   protected void execute() {
     Robot.m_cargo.activate(Robot.m_oi.getOperator());
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
+  /**The method which tells the robot if the CargoWheels.execute() command is finished
+   * @return boolean
+   */
   protected boolean isFinished() {
     return false;
   }
 
   // Called once after isFinished returns true
   @Override
+  /**The method that stop the Cargo wrist when CargoWrist.isFinished() returns true
+   * Why does this exist, isFinished() only returns false. America, explain
+   * @return void
+   */
   protected void end() {
   }
 
